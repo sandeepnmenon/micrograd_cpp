231ee29d42f20f88188f1748f2be62e1bd1c8472
Sandeep Menon <menonsandu@gmail.com>
2023-01-11 22:27:53 -0500

    training code for mlp

--- tests/nn_test.cpp
+++ tests/nn_test.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace micrograd_cpp;
 
-vector<micrograd_cpp::VariablePtr> convert_to_variable(vector<float> x)
+vector<VariablePtr> convert_to_variable(vector<float> x)
 {
     vector<VariablePtr> variables = vector<VariablePtr>{};
     for (auto i : x)
@@ -15,6 +15,51 @@ vector<micrograd_cpp::VariablePtr> convert_to_variable(vector<float> x)
     }
 
     return variables;
+}
+
+void train(MLP model)
+{
+    // // Entire Training loop
+    vector<vector<float>> xs = {
+        {2,3,-1},
+        {3,-1,0.5},
+        {0.5,1,1},
+        {1,1,-1}
+    };
+    vector<float> ys = {1,-1,-1,1};
+
+    float step_size = 0.05;
+    for(int k=0;k<100;++k)
+    {
+        //forward pass
+        vector<vector<VariablePtr>> ypred = vector<vector<VariablePtr>>();
+        for (vector<float> x : xs)
+        {
+            vector<VariablePtr> vx = convert_to_variable(x);
+            ypred.emplace_back(model(vx));
+        }
+
+        //compute loss
+        VariablePtr loss = make_shared<Variable>(0.0f);
+        for(int i = 0; i < ypred.size(); ++i)
+        {
+            loss = loss + (ypred[i][0] - ys[i]) * (ypred[i][0] - ys[i]);
+        }
+
+        model.zero_grad();
+
+        //backward pass
+        loss->backward();
+
+        //update parameters
+        for(auto p : model.parameters())
+        {
+            p->set_data(p->data() - step_size * p->grad);
+        }
+
+        if(k%10==0)
+            cout<<k<<": Loss: "<<*loss<<endl;
+    }
 }
 
 int main(int, char**) {
@@ -30,27 +75,12 @@ int main(int, char**) {
     {
         cout<<*out<<endl;
     }
-
     
-    // // Entire Training loop
-    // vector<vector<float>> xs = {
-    //     {2,3,-1},
-    //     {3,-1,0.5},
-    //     {0.5,1,1},
-    //     {1,1,-1}
-    // };
-    // vector<float> ys = {1,-1,-1,1};
+    //Define the model
+    MLP mlp = MLP(3, {4, 4, 1});
+    cout<<mlp<<" with parameters: "<<mlp.parameters().size()<<endl;
 
-    // //Define the model
-    // auto mlp = MLP(3, {4, 4, 1});
-    // cout<<mlp<<" with parameters: "<<mlp.parameters().size()<<endl;
+    train(mlp);
 
-    // float step_size = 0.05;
-    // for(int k=0;k<100;++k)
-    // {
-    //     //forward pass
-    //     auto ypred = 
-    // }
-    
     return 0;
 }
